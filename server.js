6e7a2f1ef68050390f8b1054708fe7cdc4853630
ItzHefe <jeffrey.jarvis32@gmail.com>
2022-03-20 19:46:52 -0400

    coding out routes page

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require("express");
-const fs = require("fs");
+const express = require('express');
+const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,5 +12,5 @@ app.use(express.static(__dirname));
 require('./routes')(app);
 
 app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
+    console.log('App listening on PORT: ' + PORT);
 }); 
